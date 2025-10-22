0f19774c6faec0aca4706a96078b3ab73e6d222c
LeoPhoenix05 <laelsonguerreirok9@gmail.com>
2025-10-22 00:08:28 -0300

    Add files via upload

---
+++ BFD_nivelamento1.py
@@ -0,0 +1,109 @@
+#1
+# nome="Léo"
+# print(nome)
+
+#2
+# print("Olá  Mundo!!")
+
+# 3
+# num=100
+# print (num)
+
+# 4
+# num1=7
+# num2=15
+# print(f"o primeiro numero é {num1} e o segundo numero é {num2}")
+
+# 5
+# nome="Léo"
+# idade="32"
+# print(f"nome do usuario: {nome}\nIdade do usuário: {idade}")
+
+# 6
+# num1=2
+# num2=3
+# soma=num1+num2
+# print("a soma entre o número {} e o número {} é igual a {}".format(num1, num2, soma))
+
+# 7
+# num1=5
+# num2=8
+# soma=num1+num2
+# print("a soma de {} e {} é {}".format(num1, num2, soma))
+
+# 8
+
+# palavra1=str(input("Palavra 1: "))
+# palavra2=str(input("Palavra 2: "))
+# palavra3=str(input("Palavra 3: "))
+
+# print(f"palavra1={palavra1}\npalavra2={palavra2}\npalavra3={palavra3}")
+
+# 9
+# num1=str(input("Número 1: "))
+# num2=str(input("Número 2: "))
+# num3=str(input("Número 3: "))
+
+# print(num1, num2, num3)
+
+#10 
+# print("\"Python é legal\"")
+
+#11a
+# num=int(input("quer fazer a tabuada de qual numero? "))
+# print("Iniciando a tabuada de {}".format(num))
+# cont=0
+# while cont<=10:
+#     cont+=1
+#     print("{} X {:2} = {}".format(num, cont, num*cont))
+
+#11b
+# num=int(input("quer fazer a tabuada de qual numero? "))
+# print("Iniciando a tabuada de {}".format(num))
+# print()
+# print("{} X {:2} = {}".format(num, 1, num*1))
+# print("{} X {:2} = {}".format(num, 2, num*2))
+# print("{} X {:2} = {}".format(num, 3, num*3))
+# print("{} X {:2} = {}".format(num, 4, num*4))
+# print("{} X {:2} = {}".format(num, 5, num*5))
+# print("{} X {:2} = {}".format(num, 6, num*6))
+# print("{} X {:2} = {}".format(num, 7, num*7))
+# print("{} X {:2} = {}".format(num, 8, num*8))
+# print("{} X {:2} = {}".format(num, 9, num*9))
+# print("{} X {:2} = {}".format(num, 10, num*10))
+
+#12
+# print(" ***\n ***\n *** ")
+
+#13
+# print("Aprendendo Python ")
+# print("Aprendendo Python ")
+
+#14
+# print("Python "*5)
+
+#15
+# print("eu estou aprendendo Python".upper())
+
+#16
+# num=float(input("digite um numero: "))
+# print("numero {:.2}".format(num))
+
+#17
+# num1=10
+# num2=4
+# s=num1-num2
+# print(f"10 - 4 = {s}")
+
+#18
+#print("Hello guys, my name is Léo 🐼")
+
+#19
+# print("LISTA DE COMPRAS:")
+# print("1. leite\n2. café\n3. açúcar\n4. nescau\n5. toddy")
+
+# #20
+# print("""
+# Nome: João 
+# Idade: 20 anos 
+# Cidade: Campina Grande""")
